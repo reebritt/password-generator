2a9a637df00eafbfed9ef89edd0c82fcb1dd932d
Ree Britt <reebritt@icloud.com>
2021-01-03 15:06:40 -0500

    change order of prompts and for loop

--- script.js
+++ script.js
@@ -7,33 +7,27 @@ var num ='0123456789';
 var sym = ' "!#$%&()*+,-./:;<=>?@[\]^_`{|}~';
 var passwordlength;
 
+
+//if (enter >=8 \\ enter <=128)
+prompt(To start your password selection, Choose between 8 and 128 characters);
+prompt(lowercase, uppercase, numeric, and/or special characters);
+
+for(var i = 0; i < passwordLength; i++) {
+  password += characters.charAt(Math.floor(Math.random() * characters.length));
+
+}
+
 // Write password to the #password input
 function writePassword() {
-   //if (enter >=8 \\ enter <=128)
-  prompt(To start your password selection, Choose between 8 and 128 characters);
-  prompt(lowercase, uppercase, numeric, and/or special characters);
-
-
- 
-
    var password = generatePassword()
    var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 
 }
 
-//console.log(writePassword);
-
-for (var i = 0; i < index++) {
-  const element = array[index];
-  
-}
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword){
-
-};
+generateBtn.addEventListener("click", writePassword);
 
 
 
