c06b6bea91a538121f37a5624bd92fd1b3b99d71
Ree Britt <reebritt@icloud.com>
2021-01-03 18:16:26 -0500

    password prompt accepting 8-128 updating with error if not

--- script.js
+++ script.js
@@ -4,13 +4,26 @@ var generateBtn = document.querySelector("#generate");
 // define characters , numbers, symbols to use in password generator 
 var char ='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
 var num ='0123456789';
-var sym = ' "!#$%&()*+,-./:;<=>?@[\]^_`{|}~';
-var passwordlength;
+var sym =' "!#$%&()*+,-./:;<=>?@[\]^_`{|}~';
+var passwordLength;
 
 
-//if (enter >=8 \\ enter <=128)
-prompt(To start your password selection, Choose between 8 and 128 characters);
-prompt(lowercase, uppercase, numeric, and/or special characters);
+//setup length for password and do proper checks for length check of 8-128
+function generatePassword () {
+passwordLength = prompt("To start your password selection, Choose between 8 and 128 characters");
+if (passwordLength<8) {
+  alert("Choose between 8 and 128 characters");
+  generatePassword();
+  console.log(passwordLength);
+  } else if (passwordLength>128){
+    alert("Choose between 8 and 128 characters");
+    generatePassword();
+  }
+  return passwordLength;
+console.log(passwordLength);
+};
+
+
 
 for(var i = 0; i < passwordLength; i++) {
   password += characters.charAt(Math.floor(Math.random() * characters.length));
@@ -28,7 +41,5 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
 
 //console.log(Math.floor(Math.random() * 26) +97);
